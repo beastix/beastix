4c86a8866d50c33685ba3af566d60fe98ad3f0d8
Gareth Nelson <gareh@garethnelson.com>
2015-05-21 21:58:52 +0100

    Fix for self-hosted kernel build + HD image target in releng

--- kernel/scripts/link-vmlinux.sh
+++ kernel/scripts/link-vmlinux.sh
@@ -111,7 +111,7 @@ sortextable()
 }
 
 # Delete output files in case of error
-trap cleanup SIGHUP SIGINT SIGQUIT SIGTERM ERR
+# trap cleanup SIGHUP SIGINT SIGQUIT SIGTERM ERR
 cleanup()
 {
 	rm -f .old_version
