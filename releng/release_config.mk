57cd3bbf7908029083545d19da02af55c279d23d
Gareth Nelson <gareh@garethnelson.com>
2015-05-26 05:37:26 +0100

    Update releng scripts

--- releng/release_config.mk
+++
@@ -1,5 +0,0 @@
-# Please reference the README file for information on branches
-
-BEASTIX_BRANCH=CURRENT
-BEASTIX_MAJOR=0
-BEASTIX_MINOR=1
---
+++ releng/releng-config.mk
@@ -0,0 +1,8 @@
+PWD             != pwd
+SRC_ROOT        := ${pwd}/..
+PORTS_ROOT      := ~/beastix-ports
+BEASTIX_VERSION := 0.1
+BEASTIX_BRANCH  := CURRENT
+RELEASE_BUILD   := ${SRC_ROOT}/releng/build
+RELEASE_OUTPUT  := ${SRC_ROOT}/releng/release
+RELEASE_FINAL   := ${SRC_ROOT}/releng/release/Beastix-${BEASTIX_VERSION}-${BEASTIX_BRANCH}
