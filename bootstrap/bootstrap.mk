8d42b2a13a93eb3a6e7511ab383edc01819d7780
Gareth Nelson <gareh@garethnelson.com>
2015-05-26 22:18:58 +0100

    Compile musl and util-linux with -fPIC

--- bootstrap/bootstrap.mk
+++ bootstrap/bootstrap.mk
@@ -3,7 +3,7 @@ bootstrap-binutils:
 	make -C ${SRC_ROOT}/bootstrap/binutils all
 	make -C ${SRC_ROOT}/bootstrap/binutils install-gas install-ld install-binutils
 
-bootstrap-gcc: bootstrap-binutils
+bootstrap-gcc:
 	cd ${SRC_ROOT}/bootstrap/gcc; ${SRC_ROOT}/world/gcc/configure ${BOOTSTRAP_CONFIG} --build=x86_64-unknown-linux-musl --enable-languages=c --with-newlib --disable-multilib \
                                                                                           --disable-libssp --disable-libquadmath --disable-threads --disable-decimal-float --disable-shared \
                                                                                           --disable-libmudflap --disable-libgomp
@@ -11,13 +11,15 @@ bootstrap-gcc: bootstrap-binutils
 	make -C ${SRC_ROOT}/bootstrap/gcc all-target-libgcc install-gcc install-target-libgcc
 	cd ${BOOTSTRAP_TOOLS}/bin; ln -sf x86_64-unknown-linux-musl-gcc x86_64-unknown-linux-musl-cc
 
-bootstrap-musl: bootstrap-gcc bootstrap-binutils
-	export PATH=${BOOTSTRAP_PATH}; export CC=${BOOTSTRAP_CC}; cd ${SRC_ROOT}/world/musl; ./configure ${BOOTSTRAP_CONFIG} --syslibdir=${BOOTSTRAP_TOOLS}/lib --host="x86_64-unknown-linux-musl" \
+bootstrap-musl:
+	export PATH=${BOOTSTRAP_PATH}; export CC=${BOOTSTRAP_CC}; export CFLAGS=-fPIC; cd ${SRC_ROOT}/world/musl; ./configure ${BOOTSTRAP_CONFIG} --syslibdir=${BOOTSTRAP_TOOLS}/lib \
+	                                                                                                                    --host="x86_64-unknown-linux-musl" \
 	                                                                                                                    --disable-gcc-wrapper --disable-shared --enable-static
 	export PATH=${BOOTSTRAP_PATH}; export CC=${BOOTSTRAP_CC}; make -C ${SRC_ROOT}/world/musl install
 
 bootstrap-util-linux:
-	cd ${SRC_ROOT}/bootstrap/util-linux; ${SRC_ROOT}/world/util-linux/configure ${BOOTSTRAP_CONFIG} --prefix=${WORLD_BUILD}/util-linux/_install --disable-nls --enable-static --disable-rpath \
+	export CFLAGS=-fPIC; cd ${SRC_ROOT}/bootstrap/util-linux; ${SRC_ROOT}/world/util-linux/configure ${BOOTSTRAP_CONFIG} --prefix=${WORLD_BUILD}/util-linux/_install --disable-nls --enable-static \
+	                                                                                                 --disable-rpath \
 	                                                                                                 --disable-all-programs --disable-bash-completion --disable-makeinstall-setuid --without-selinux \
 	                                                                                                 --without-udev --without-libiconv --without-libintl-prefix --without-slang \
 	                                                                                                 --without-ncurses --without-utempter --without-user --without-systemd --without-smack \
