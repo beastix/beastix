03967be9cc7de4ede1799d58da40fd5cc6b2b226
Gareth Nelson <gareh@garethnelson.com>
2015-05-27 06:42:58 +0100

    Enable unattended builds

--- kernel/kernel.mk
+++ kernel/kernel.mk
@@ -2,7 +2,6 @@ buildkernel:
 	make -C kernel/ mrproper
 	make -C kernel defconfig O=../obj/kernel
 	sed -i "s/.*CONFIG_DEFAULT_HOSTNAME.*/CONFIG_DEFAULT_HOSTNAME=\"beastix\"/" obj/kernel/.config
-	sed -i "s/.*CONFIG_LOCALVERSION.*/CONFIG_LOCALVERSION=\"${BEASTIX_VERSION}\"/" obj/kernel/.config
 	make -C kernel bzImage O=../obj/kernel/ 
 
 installkernel:
