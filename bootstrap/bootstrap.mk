fa207c908a952bdb61e954ed5abb42b1ea60fba2
Gareth Nelson <gareh@garethnelson.com>
2015-05-27 09:30:26 +0100

    fix for bootstrapping on gcc 4.9

--- bootstrap/bootstrap.mk
+++ bootstrap/bootstrap.mk
@@ -1,12 +1,12 @@
 bootstrap-binutils:
-	cd ${SRC_ROOT}/bootstrap/binutils; ${SRC_ROOT}/world/binutils/configure ${BOOTSTRAP_CONFIG}
+	cd ${SRC_ROOT}/bootstrap/binutils; ${SRC_ROOT}/world/binutils/configure ${BOOTSTRAP_CONFIG} --disable-werror
 	${MAKE} -C ${SRC_ROOT}/bootstrap/binutils all
 	${MAKE} -C ${SRC_ROOT}/bootstrap/binutils install-gas install-ld install-binutils
 
 bootstrap-gcc:
 	cd ${SRC_ROOT}/bootstrap/gcc; ${SRC_ROOT}/world/gcc/configure ${BOOTSTRAP_CONFIG} --build=x86_64-unknown-linux-musl --enable-languages=c --with-newlib --disable-multilib \
                                                                                           --disable-libssp --disable-libquadmath --disable-threads --disable-decimal-float --disable-shared \
-                                                                                          --disable-libmudflap --disable-libgomp
+                                                                                          --disable-libmudflap --disable-libgomp --disable-werror
 	make -C ${SRC_ROOT}/bootstrap/gcc all-gcc install-gcc
 	make -C ${SRC_ROOT}/bootstrap/gcc all-target-libgcc install-gcc install-target-libgcc
 	cd ${BOOTSTRAP_TOOLS}/bin; ln -sf x86_64-unknown-linux-musl-gcc x86_64-unknown-linux-musl-cc
