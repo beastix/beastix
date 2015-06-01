87c2038f03e80d22d0b0201db2b3cd5d6e68f366
Gareth Nelson <gareh@garethnelson.com>
2015-06-01 07:17:45 +0100

    touch ldlinux.bin

--- world/world.mk
+++ world/world.mk
@@ -48,6 +48,7 @@ buildworld-util-linux:
 
 buildworld-syslinux:
 	mkdir -p ${WORLD_BUILD}/syslinux/_install
+	touch ${WORLD_BUILD}/syslinux/bios/core/ldlinux.bin
 	${WORLDENV} make -C ${SRC_ROOT}/world/syslinux CC=${WORLD_CC} O=${WORLD_BUILD}/syslinux 
 	${WORLDENV} make -C ${SRC_ROOT}/world/syslinux CC=${WORLD_CC} install O=${WORLD_BUILD}/syslinux INSTALLROOT=${WORLD_BUILD}/syslinux/_install
 
